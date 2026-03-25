7843bf51445b73f457e4f073cb8f71ee01bbedd0
renaeliving <renaeliving@gmail.com>
2026-03-24 22:33:11 -0700

    Update prismaClient.js

--- prismaClient.js
+++ prismaClient.js
@@ -1,5 +1,3 @@
-import { prisma } from "./prismaClient.js";
-import { supabaseAdmin } from "./supabaseClient.js";
 import { PrismaClient } from "@prisma/client";
 
 export const prisma = new PrismaClient();
